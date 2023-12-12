915e592672784fce9d3495be360ea5001e9d7045
dev1 <dev1@email.com>
2023-12-12 10:35:18 +0000

    this is my second server launch

---
+++ variable1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:hi good moring "
+echo "$PERSON2:hello good moring"
+echo "$PERSON1:how r u $PERSON2"
+echo  "$PERSON2:iam good $PERSON1,how r u"
+echo "$PERSON1:iam also good $PERSON2,thankyou"
