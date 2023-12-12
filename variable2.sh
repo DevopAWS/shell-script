ad1c79e52f7af168b2a9d83c6662e7ab6847ed2e
dev1 <dev1@email.com>
2023-12-12 11:03:29 +0000

    third launching server

--- variable2.sh
+++ variable2.sh
@@ -4,12 +4,12 @@ echo username:${USERNAME}
 echo password:${PASSWORD}
 
 echo -n "enter your username:"
-read -s $USERNAME
+read -s USERNAME
 
 echo
 
 echo -n "enter your password:"
-read -s $PASSWORD
+read -s PASSWORD
 
 echo
 
