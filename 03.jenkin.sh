64c18af1056de33d94128b138eb17d27472f9333
dev1 <dev1@email.com>
2023-12-13 12:46:32 +0000

    jenkin install

--- 03.jenkin.sh
+++ 03.jenkin.sh
@@ -10,16 +10,14 @@ N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-
-
-VALIDATE(){
-    if [ $1 -ne 0 ];then
-        echo "$2....$R FAILURE $N"
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2....$R FAILURE $N"
             exit 1
             else 
-                echo "$2....$G SUCCESS $N"
-
+                echo -e "$2....$G SUCCESS $N"
 }
+
 
 if [ $id -ne 0 ];then
     echo "ERROR::script excuting with root user"
@@ -29,38 +27,38 @@ if [ $id -ne 0 ];then
     fi
 
 
+echo "excuting scrpit start $TIMESTAMP"  &>> $LOGFILE
 
 
-
-yum update -y
+yum update -y &>> $LOGFILE
 
 VALIDATE $? "yum update is.."
 
 wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo &>> $LOGFILE
 
     VALIDATE $? "repo is "
 
-    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key 
+    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key  &>> $LOGFILE
 
     VALIDATE $? "importing package is "
 
-    amazon-linux-extras install java-openjdk11 -y
+    amazon-linux-extras install java-openjdk11 -y &>> $LOGFILE
 
     VALIDATE $? "java install"
 
-    yum install jenkins -y
+    yum install jenkins -y &>> $LOGFILE
 
     VALIDATE $? "jenkin install is"
 
-    systemctl enable jenkins
+    systemctl enable jenkins &>> $LOGFILE
 
     VALIDATE $? "jenkin enable is"
 
-    systemctl start jenkins
+    systemctl start jenkins &>> $LOGFILE
 
-    VALIDATE $? "jenkin start"
+    VALIDATE $? "jenkin start" 
 
-    systemctl status jenkins
+    systemctl status jenkins &>> $LOGFILE
 
     VALIDATE $? "jenkin status is"
