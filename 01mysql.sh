4ea27717411bdb860c252103b1f8ee5151ff3362
dev1 <dev1@email.com>
2023-12-13 10:36:15 +0000

    sql install

--- 01mysql.sh
+++ 01mysql.sh
@@ -6,8 +6,15 @@ if [ $ID -ne 0 ];then
     echo "ERROR::please run the script root user"
     exit 1
         else
-            echo "you are not root user"
+            echo "you are root user"
 fi
 
 yum install mysql -y
+
+if [ $ID -ne 0 ];then
+    echo "Error::mysql installed falilue"
+    exit 1
+        else 
+        echo "mysql installed success'
+fi
 
