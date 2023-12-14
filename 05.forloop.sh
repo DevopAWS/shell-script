bc69b87f7fe38db9d293dec5fddaefd61bd28388
dev1 <dev1@email.com>
2023-12-14 11:03:25 +0000

    for loop

--- 05.forloop.sh
+++ 05.forloop.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
-USERID=$(id -u)
+ID=($id -u)
 
 TIMESTAMP=$(date +%F)
 
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
 R="\e[31m"
 G="\e[32m"
-Y="\e[33"
 N="\e[0m"
+Y="\e[33m"
 
-VALIDATE(){
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-    if [ $1 -ne 0 ];then
-        echo -e "$2....$R FAILURE $N"
-        exit 1
-            else
-            echo -e"$2..$R SUCCESS $N"
+echo "excuting scrpit start $TIMESTAMP"  &>> $LOGFILE
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2....$R FAILURE.. $N"
+            exit 1
+            else 
+                echo -e "$2....$G SUCCESS.. $N"
     fi
 }
 
-if [ $USERID -ne 0 ];then
-    echo -e "$R excuting script with root user $N"
-    exit 1
-        else
-            echo -e "$Y you are root user $N"
-fi
+
+if [ $id -ne 0 ]; then
+    echo "ERROR::script excuting with root user"
+        exit 1
+             else
+                echo "yor are root user"
+    fi
 
 for package in $@
     do
-        yum list installed $package 
+        yum list installed $package  &>> $LOGFILE
         if [ $? -ne 0 ];then
             yum install $package -y &>> $LOGFILE
             VALIDATE $? "installation of $package"
